565aa6c7a89948d51861eed9063047a9e334b464
Skydark Chen <skydark2@gmail.com>
2015-11-09 23:35:45 +0800

    Make maids laugh when give her flowers

--- src/main/java/info/skydark/maidpro/EventHook.java
+++ src/main/java/info/skydark/maidpro/EventHook.java
@@ -1,12 +1,14 @@
 package info.skydark.maidpro;
 
 import littleMaidMobX.*;
+import net.minecraft.block.BlockFlower;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.IEntityOwnable;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
@@ -21,21 +23,26 @@ public class EventHook
 {
     @SubscribeEvent
     public void interact(EntityInteractEvent event) {
-        if (Config.milkmode == 0 || !(event.target instanceof LMM_EntityLittleMaid)) {
+        if (!(event.target instanceof LMM_EntityLittleMaid)) {
             return;
         }
         EntityPlayer player = event.entityPlayer;
         LMM_EntityLittleMaid maid = (LMM_EntityLittleMaid) event.target;
         ItemStack itemstack = player.inventory.getCurrentItem();
-        if (itemstack != null && itemstack.getItem() == Items.bucket && player.isSneaking()) {
-            if (Config.milkmode == 2 || (maid.isContractEX() && maid.isMaidContractOwner(player))) {
-                if (!player.capabilities.isCreativeMode && --itemstack.stackSize <= 0) {
-                    player.inventory.setInventorySlotContents(player.inventory.currentItem, new ItemStack(Items.milk_bucket));
-                } else if (!player.inventory.addItemStackToInventory(new ItemStack(Items.milk_bucket))) {
-                    player.dropPlayerItemWithRandomChoice(new ItemStack(Items.milk_bucket), false);
+        if (itemstack != null && player.isSneaking()) {
+            Item item = itemstack.getItem();
+            if (Config.milkmode != 0 && item == Items.bucket) {
+                if (Config.milkmode == 2 || (maid.isContractEX() && maid.isMaidContractOwner(player))) {
+                    if (!player.capabilities.isCreativeMode && --itemstack.stackSize <= 0) {
+                        player.inventory.setInventorySlotContents(player.inventory.currentItem, new ItemStack(Items.milk_bucket));
+                    } else if (!player.inventory.addItemStackToInventory(new ItemStack(Items.milk_bucket))) {
+                        player.dropPlayerItemWithRandomChoice(new ItemStack(Items.milk_bucket), false);
+                    }
                 }
+                maid.playSound(LMM_EnumSound.living_whine, false);
+            } else if (item instanceof ItemBlock && ((ItemBlock) item).field_150939_a instanceof BlockFlower && maid.getRNG().nextInt(3) == 0) {
+                maid.playSound(LMM_EnumSound.laughter, false);
             }
-            ((LMM_EntityLittleMaid) event.target).playSound(LMM_EnumSound.living_whine, false);
         }
     }
 
