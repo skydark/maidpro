e5eb8dc9411b60ddc2a33603849916ab9cc07752
Skydark Chen <skydark2@gmail.com>
2015-11-11 13:58:37 +0800

    Move iron golem's ai to a separate class

---
+++ src/main/java/info/skydark/maidpro/EntityAILookAtVillagerAndMaid.java
@@ -0,0 +1,75 @@
+package info.skydark.maidpro;
+
+import littleMaidMobX.LMM_EntityLittleMaid;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.ai.EntityAIBase;
+import net.minecraft.entity.monster.EntityIronGolem;
+import net.minecraft.entity.passive.EntityVillager;
+
+/**
+ * Created by skydark on 15-11-11.
+ */
+public class EntityAILookAtVillagerAndMaid extends EntityAIBase {
+    private EntityIronGolem theGolem;
+    private Entity lookingAt;
+    private int lookTime;
+    private static final String __OBFID = "CL_00001602";
+
+    public EntityAILookAtVillagerAndMaid(EntityIronGolem p_i1643_1_) {
+        this.theGolem = p_i1643_1_;
+        this.setMutexBits(3);
+    }
+
+    /**
+     * Returns whether the EntityAIBase should begin execution.
+     */
+    public boolean shouldExecute() {
+        if (!this.theGolem.worldObj.isDaytime() && this.theGolem.dimension != -1) {
+            return false;
+        } else if (this.theGolem.getRNG().nextInt(8000) != 0) {
+            return false;
+        } else {
+            EntityVillager theNearestVillager = (EntityVillager) this.theGolem.worldObj.findNearestEntityWithinAABB(EntityVillager.class, this.theGolem.boundingBox.expand(6.0D, 2.0D, 6.0D), this.theGolem);
+            LMM_EntityLittleMaid theNearestMaid = (LMM_EntityLittleMaid) this.theGolem.worldObj.findNearestEntityWithinAABB(LMM_EntityLittleMaid.class, this.theGolem.boundingBox.expand(6.0D, 2.0D, 6.0D), this.theGolem);
+            if (theNearestVillager == null) {
+                lookingAt = theNearestMaid;
+            } else if (theNearestMaid == null) {
+                lookingAt = theNearestVillager;
+            } else {
+                lookingAt = this.theGolem.getDistanceSqToEntity(theNearestVillager) > this.theGolem.getDistanceSqToEntity(theNearestMaid) ? theNearestMaid : theNearestVillager;
+            }
+            return this.lookingAt != null;
+        }
+    }
+
+    /**
+     * Returns whether an in-progress EntityAIBase should continue executing
+     */
+    public boolean continueExecuting() {
+        return this.lookTime > 0;
+    }
+
+    /**
+     * Execute a one shot task or start executing a continuous task
+     */
+    public void startExecuting() {
+        this.lookTime = 400;
+        this.theGolem.setHoldingRose(true);
+    }
+
+    /**
+     * Resets the task
+     */
+    public void resetTask() {
+        this.theGolem.setHoldingRose(false);
+        this.lookingAt = null;
+    }
+
+    /**
+     * Updates the task
+     */
+    public void updateTask() {
+        this.theGolem.getLookHelper().setLookPositionWithEntity(this.lookingAt, 30.0F, 30.0F);
+        --this.lookTime;
+    }
+}
--- src/main/java/info/skydark/maidpro/EventHookIronGolem.java
+++ src/main/java/info/skydark/maidpro/EventHookIronGolem.java
@@ -1,13 +1,10 @@
 package info.skydark.maidpro;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-import littleMaidMobX.LMM_EntityLittleMaid;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.ai.EntityAIBase;
 import net.minecraft.entity.ai.EntityAILookAtVillager;
 import net.minecraft.entity.ai.EntityAITasks;
 import net.minecraft.entity.monster.EntityIronGolem;
-import net.minecraft.entity.passive.EntityVillager;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 
 /**
@@ -16,7 +13,6 @@ import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 public class EventHookIronGolem {
     @SubscribeEvent
     public void onEntitySpawn(EntityJoinWorldEvent event) {
-
         if (event.entity instanceof EntityIronGolem) {
             EntityIronGolem entity = (EntityIronGolem) event.entity;
             for (Object entry : entity.tasks.taskEntries.toArray()) {
@@ -27,71 +23,6 @@ public class EventHookIronGolem {
                     break;
                 }
             }
-        }
-    }
-
-    public class EntityAILookAtVillagerAndMaid extends EntityAIBase {
-        private EntityIronGolem theGolem;
-        private Entity lookingAt;
-        private int lookTime;
-        private static final String __OBFID = "CL_00001602";
-
-        public EntityAILookAtVillagerAndMaid(EntityIronGolem p_i1643_1_) {
-            this.theGolem = p_i1643_1_;
-            this.setMutexBits(3);
-        }
-
-        /**
-         * Returns whether the EntityAIBase should begin execution.
-         */
-        public boolean shouldExecute() {
-            if (!this.theGolem.worldObj.isDaytime() && this.theGolem.dimension != -1) {
-                return false;
-            } else if (this.theGolem.getRNG().nextInt(8000) != 0) {
-                return false;
-            } else {
-                EntityVillager theNearestVillager = (EntityVillager) this.theGolem.worldObj.findNearestEntityWithinAABB(EntityVillager.class, this.theGolem.boundingBox.expand(6.0D, 2.0D, 6.0D), this.theGolem);
-                LMM_EntityLittleMaid theNearestMaid = (LMM_EntityLittleMaid) this.theGolem.worldObj.findNearestEntityWithinAABB(LMM_EntityLittleMaid.class, this.theGolem.boundingBox.expand(6.0D, 2.0D, 6.0D), this.theGolem);
-                if (theNearestVillager == null) {
-                    lookingAt = theNearestMaid;
-                } else if (theNearestMaid == null) {
-                    lookingAt = theNearestVillager;
-                } else {
-                    lookingAt = this.theGolem.getDistanceSqToEntity(theNearestVillager) > this.theGolem.getDistanceSqToEntity(theNearestMaid) ? theNearestMaid : theNearestVillager;
-                }
-                return this.lookingAt != null;
-            }
-        }
-
-        /**
-         * Returns whether an in-progress EntityAIBase should continue executing
-         */
-        public boolean continueExecuting() {
-            return this.lookTime > 0;
-        }
-
-        /**
-         * Execute a one shot task or start executing a continuous task
-         */
-        public void startExecuting() {
-            this.lookTime = 400;
-            this.theGolem.setHoldingRose(true);
-        }
-
-        /**
-         * Resets the task
-         */
-        public void resetTask() {
-            this.theGolem.setHoldingRose(false);
-            this.lookingAt = null;
-        }
-
-        /**
-         * Updates the task
-         */
-        public void updateTask() {
-            this.theGolem.getLookHelper().setLookPositionWithEntity(this.lookingAt, 30.0F, 30.0F);
-            --this.lookTime;
         }
     }
 }
