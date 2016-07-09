3ab723a3a98b41e5d3a64a42574d92e2dcac970f
Skydark Chen <skydark2@gmail.com>
2016-07-10 01:32:07 +0800

    Prevent tamed wolves attacking maids due to wilds
    Maids: `Love me or those wild wolves?'
    Doges: `such Album, very White'

--- src/main/java/info/skydark/maidpro/EventHookWolf.java
+++ src/main/java/info/skydark/maidpro/EventHookWolf.java
@@ -4,9 +4,13 @@ import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import info.skydark.maidpro.ai.EntityAIHurtByTargetPlus;
 import info.skydark.maidpro.ai.EntityAIOwnerHurtByTargetPlus;
 import info.skydark.maidpro.ai.EntityAIOwnerHurtTargetPlus;
+import littleMaidMobX.LMM_EntityLittleMaid;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.ai.*;
 import net.minecraft.entity.passive.EntityWolf;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.event.entity.living.LivingSetAttackTargetEvent;
 
 /**
  * Created by skydark on 15-11-11.
@@ -25,6 +29,22 @@ public class EventHookWolf {
             entity.targetTasks.addTask(1, new EntityAIOwnerHurtByTargetPlus(entity));
             entity.targetTasks.addTask(2, new EntityAIOwnerHurtTargetPlus(entity));
             entity.targetTasks.addTask(3, new EntityAIHurtByTargetPlus(entity, true));
+        }
+    }
+
+    @SubscribeEvent
+    public void onSetAttackTarget(LivingSetAttackTargetEvent event) {
+        if (!(event.entity instanceof EntityWolf))
+            return;
+        EntityWolf wolf = (EntityWolf) event.entity;
+        EntityLivingBase target = wolf.getAttackTarget();
+        if (target == null || !(target instanceof LMM_EntityLittleMaid))
+            return;
+        EntityLivingBase owner = wolf.getOwner();
+        if (owner instanceof EntityPlayer && ((LMM_EntityLittleMaid) target).isMaidContractOwner((EntityPlayer) owner)) {
+            wolf.setAttackTarget(null);
+            wolf.setTarget(null);
+            wolf.setRevengeTarget(null);
         }
     }
 }
